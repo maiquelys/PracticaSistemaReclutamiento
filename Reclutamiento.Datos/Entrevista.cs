5178b1dd3bd2a5e7a8f692e7ccfa5ac27da5881a
Maiquelys González <maiquelysgonzalez@gmail.com>
2026-06-28 13:32:03 -0400

    class entrevista

---
+++ Reclutamiento.Datos/Entrevista.cs
@@ -0,0 +1,27 @@
+﻿namespace Reclutamiento.Datos
+{
+    public class Entrevista
+    {
+        public int EntrevistaID { get; set; }
+        public int CandidatoID { get; set; }
+        public int VacanteID { get; set; }
+        public int EtapaID { get; set; }
+        public DateTime Fecha { get; set; }
+        public decimal Puntuacion { get; set; }
+
+        //constructor
+        public Entrevista(int candidatoID, int vacanteID, int etapaID, decimal ountuacion)
+        {
+            this.CandidatoID = candidatoID;
+            this.VacanteID = vacanteID;
+            this.EtapaID = etapaID;
+            this.Fecha = DateTime.Now;
+            this.Puntuacion = Puntuacion;
+        }
+        public Entrevista()
+        {
+            this.Fecha = DateTime.Now;
+            this.Puntuacion = 0;
+        }
+    }
+}
